4648b5bf896d07b83afdbf86086f29b2b60312cc
sujangowda077 <kingmaker999999991@gmail.com>
2026-02-13 15:22:21 +0530

    Update config.ts

--- build-template/src/frontend/src/landing/config.ts
+++ build-template/src/frontend/src/landing/config.ts
@@ -1,12 +1,12 @@
 export const LANDING_CONFIG = {
-  downloadUrl: 'https://drive.google.com/file/d/1xRBFDXkyTBB47x1NL3gdoPprM5y_3_iv/view?usp=sharing',
+  downloadUrl: 'https://drive.google.com/file/d/14NFkSZzxTgwh_zYXZAj_kVFWuFAS9IgO/view?usp=sharing',
   openInNewTab: true,
   contactEmail: 'office.sidequest26@gmail.com',
   privacyPolicyUrl: '#privacy',
   termsUrl: '#terms',
   social: {
-    twitter: 'https://twitter.com/sidequest',
+    twitter: 'https://twitter.com',
     instagram: 'https://www.instagram.com/side_quest26/',
-    linkedin: 'https://linkedin.com/company/sidequest'
+    linkedin: 'https://linkedin.com/company'
   }
 };
