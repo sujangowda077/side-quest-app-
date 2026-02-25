6c4fb5da2f9cf30e29ab5669f3a4f2da6f1a358d
sujangowda077 <kingmaker999999991@gmail.com>
2026-02-25 14:43:30 +0530

    Update sidequest.ts

--- build-template/src/frontend/src/config/sidequest.ts
+++ build-template/src/frontend/src/config/sidequest.ts
@@ -1,5 +1,5 @@
 // SideQuest app configuration
 export const SIDEQUEST_CONFIG = {
   // Update this URL to your app download link
-  downloadUrl: 'https://drive.google.com/file/d/1lvJfsh4ow3FoskSs-ACk2j4zkpu_4CTy/view?usp=drivesdk',
+  downloadUrl: 'https://drive.google.com/file/d/1lvJfsh4ow3FoskSs-ACk2j4zkpu_4CTy/view?usp=sharing',
 } as const;
