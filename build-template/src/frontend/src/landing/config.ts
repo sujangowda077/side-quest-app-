22b1d1216eafe06ee50be9b4bc8a01ce97c1ea90
sujangowda077 <kingmaker999999991@gmail.com>
2026-02-25 14:42:38 +0530

    Update config.ts

--- build-template/src/frontend/src/landing/config.ts
+++ build-template/src/frontend/src/landing/config.ts
@@ -1,6 +1,6 @@
 export const LANDING_CONFIG = {
   appName: 'SideQuest',
-  downloadUrl: 'https://drive.google.com/file/d/1lvJfsh4ow3FoskSs-ACk2j4zkpu_4CTy/view?usp=drivesdk',
+  downloadUrl: 'https://drive.google.com/file/d/1lvJfsh4ow3FoskSs-ACk2j4zkpu_4CTy/view?usp=sharing',
   openInNewTab: true,
   contactEmail: 'office.sidequest26@gmail.com',
   privacyPolicyUrl: '#privacy',
